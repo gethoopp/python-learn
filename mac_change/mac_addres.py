8272d1d339b386e5573d1befbff5edf7abc7c4d2
gethoopp <halim.keren1@gmail.com>
2025-11-27 10:41:44 +0700

    handle input user

--- mac_change/mac_addres.py
+++ mac_change/mac_addres.py
@@ -1,7 +1,12 @@
 import subprocess
 
-
+#this function define to change mac adrres
 def mac_changer():
+    #handle input interface 
+    interface = input("input the interface >");
+    #mac_addres = " 04:03:01:a2:a5:a6";
+    #handel input new mac addres
+    mac_addres = input("Input the new addres >");
     subprocess.run(
         "ifconfig", shell=True, text=True,check=True,capture_output=True
     )
@@ -9,23 +14,23 @@ def mac_changer():
 
 
     subprocess.run(
-        " ifconfig eth0 down", shell=True, text=True, check=True,capture_output=True
+        f" ifconfig {interface} down", shell=True, text=True, check=True,capture_output=True
     )
 
  
 
     subprocess.run(
-        "ifconfig eth0 down", shell=True, text=True, check=True,capture_output=True
+        f"ifconfig {interface} down", shell=True, text=True, check=True,capture_output=True
     )
 
 
     
     subprocess.run(
-        "ifconfig eth0 hw ether 04:03:01:a2:a5:a6",shell=True,text=True,check=True,capture_output=True
+        f"ifconfig {interface} hw ether {mac_addres}",shell=True,text=True,check=True,capture_output=True
     )
 
     subprocess.run(
-        "ifconfig eth0 up",shell=True,text=True,check=True,capture_output=True
+        f"ifconfig {interface} up",shell=True,text=True,check=True,capture_output=True
     )
     
     print("succes change the mac adress let boom it")
